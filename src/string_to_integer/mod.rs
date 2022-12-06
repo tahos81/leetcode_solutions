f5f38ecd76b4709017925b0eb1fcdc90af4d3b8c
tahos81 <tahirozpala@gmail.com>
2022-12-06 18:20:20 +0300

    update string-to-integer

--- src/string_to_integer/mod.rs
+++ src/string_to_integer/mod.rs
@@ -3,7 +3,7 @@ use std::num::ParseIntError;
 
 pub fn my_atoi(s: String) -> i32 {
     let trimmed_s: &str = s.trim_start();
-    let x: Option<&str> = trimmed_s
+    let string_to_parse: Option<&str> = trimmed_s
         .find(|character: char| !character.is_numeric())
         .and_then(|index: usize| match index {
             //okey to use unwrap() since it can not panic
@@ -16,7 +16,8 @@ pub fn my_atoi(s: String) -> i32 {
             _ => Some(&trimmed_s[..index]),
         });
 
-    x.unwrap_or(trimmed_s)
+    string_to_parse
+        .unwrap_or(trimmed_s)
         .parse::<i32>()
         .unwrap_or_else(|err: ParseIntError| match err.kind() {
             std::num::IntErrorKind::PosOverflow => MAX,
